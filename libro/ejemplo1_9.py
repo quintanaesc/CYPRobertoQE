cfbe71b14e249ff4e5dbe451623bb9f3f300dbe7
quintanaesc <2im3.quintana.esc.azul@gmail.com>
2019-09-12 17:11:31 -0500

    se corrige un error en el texto de ejemplo1_9.py

--- libro/ejemplo1_9.py
+++ libro/ejemplo1_9.py
@@ -1,5 +1,5 @@
 num= int(input("dame un numero entero:"))
 cua = num * num
 cub = num ** 3
-print(f"el cuadradi de {num} esl {cua}")
+print(f"el cuadradi de {num} es el {cua}")
 print(f"el cubo de {num} es {cub}")
