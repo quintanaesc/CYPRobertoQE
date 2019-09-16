2eced5d022fd37c4abd6c9fcae05e277bb25f464
quintanaesc <2im3.quintana.esc.azul@gmail.com>
2019-09-16 00:05:08 -0500

    Se creo ejemplo1_11.py funcional

---
+++ libro/ejemplo1_11.py
@@ -0,0 +1,5 @@
+A= int(input("Dame un numero entero"))
+B= int(input("Dame otro numero entero"))
+C= int(input("Dame un tercer numero entero"))
+D= int(input("Dame un ultimo numero entero"))
+print(f"El numero generado ordenandolos del ultimo al primero es {D} {C} {B} {A}")
