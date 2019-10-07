2180c3c90b05889d264754376953d90021616604
quintanaesc <2im3.quintana.esc.azul@gmail.com>
2019-10-07 12:07:49 -0500

    Se practica creando el archico problema2_5.py no funcional

---
+++ libro/problemasresueltos/capitulo2/problema2_5.py
@@ -0,0 +1,8 @@
+NUM=int(input("Ingrese un numero entero "))
+if NUM<0:
+	print(f"El numero {NUM} es negativo")
+if NUM=1:
+	print(f"El numero {NUM} es nulo")
+if NUM>0:
+	print(f"El numero {NUM} es positivo")
+print("fin del programa")
