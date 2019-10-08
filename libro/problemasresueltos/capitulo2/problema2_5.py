00e32c6fd0006b00827788cda86b87e39266dfbb
quintanaesc <2im3.quintana.esc.azul@gmail.com>
2019-10-08 17:53:07 -0500

    Se reparo el archivo problema2_5.py

--- libro/problemasresueltos/capitulo2/problema2_5.py
+++ libro/problemasresueltos/capitulo2/problema2_5.py
@@ -1,8 +1,8 @@
-NUM=int(input("Ingrese un numero entero "))
+NUM=float(input("Ingrese un numero entero "))
 if NUM<0:
 	print(f"El numero {NUM} es negativo")
-if NUM=1:
-	print(f"El numero {NUM} es nulo")
+if NUM==0:
+	print(f"el numero {NUM} es nulo")
 if NUM>0:
 	print(f"El numero {NUM} es positivo")
 print("fin del programa")
