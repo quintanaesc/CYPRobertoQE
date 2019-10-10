5fb90f9a35c8a1c6771f3b07a9bdb3a194a465d4
quintanaesc <2im3.quintana.esc.azul@gmail.com>
2019-10-10 17:48:17 -0500

    Se agregan ejemplos de la estructura for: for.py y for2.py

---
+++ for.py
@@ -0,0 +1,4 @@
+V=0
+for V in range(1,11,1):
+    W=V*5
+    print(f"5x{V}={W}")
---
+++ for2.py
@@ -0,0 +1,5 @@
+tabla=int(input("¿Que tabla?: "))
+V=0
+for V in range(1,11,1):
+    W=tabla*V
+    print(f"{tabla}x{V}={W}")
