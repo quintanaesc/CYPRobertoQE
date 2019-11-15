c919162052e0561ceb0ccd2f63cf4c8392db78b5
quintanaesc <2im3.quintana.esc.azul@gmail.com>
2019-11-14 23:30:44 -0600

    se termina el archivo Banner.py funcional en terminal

--- banner.py
+++ banner.py
@@ -1,4 +1,17 @@
-from color import red
-print(red("asd"))
-from banner import Banner
-print(Banner("BOOOOOOOMMMM!!"))
+#banner de color en una terminal
+from asciistuff import Banner
+import os
+os.system("cls")
+
+print("\u001b[33m")
+print(Banner("Universidad"))
+print(Banner("Autonoma"))
+print(Banner("de Mexico"))
+
+
+print("\u001b[34m")
+print(Banner("FES Aragon"))
+
+print("\u001b[32m")
+print(Banner("Quintana Escamilla"))
+print(Banner("Roberto Carlos"))
